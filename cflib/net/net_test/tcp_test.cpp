f9f84e036348a8247d6a8ddd5bc432be0f862d76
Christian Fischbach <cfischbach@mac.com>
2015-08-22 11:11:48 +0200

    started tcp_test

---
+++ cflib/net/net_test/tcp_test.cpp
@@ -0,0 +1,36 @@
+/* Copyright (C) 2013-2014 Christian Fischbach <cf@cflib.de>
+ *
+ * This file is part of cflib.
+ *
+ * cflib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cflib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cflib. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cflib/util/test.h>
+#include <cflib/util/util.h>
+
+using namespace cflib::util;
+
+class TCP_Test: public QObject
+{
+	Q_OBJECT
+private slots:
+
+	void test_simple()
+	{
+		QVERIFY(true);
+	}
+
+};
+#include "tcp_test.moc"
+ADD_TEST(TCP_Test)
