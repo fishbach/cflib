0aef1eae8c96205f157f26bdd6eef013f265c25e
Christian Fischbach <cfischbach@mac.com>
2015-09-30 11:47:31 +0200

    added extend

--- js/dom.js
+++ js/dom.js
@@ -139,6 +139,8 @@ define(function() {
 		return $rv.matches(sel, scope) ? $rv : null;
 	};
 
+	$.extend = function(obj1, obj2) { for (var key in obj2) obj1[key] = obj2[key]; };
+
 	// ------------------------------------------------------------------------
 
 	$.fn = Fn.prototype = {
