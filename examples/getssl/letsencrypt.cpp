a3c9460c19256bb411395d1df5136c891090e40e
Christian Fischbach <cfischbach@mac.com>
2017-05-08 15:47:26 +0200

    fixed lets encrypt agreement url

--- examples/getssl/letsencrypt.cpp
+++ examples/getssl/letsencrypt.cpp
@@ -33,7 +33,7 @@ using namespace cflib::util;
 
 namespace {
 
-const QByteArray LetsencryptAgreement = "https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf";
+const QByteArray LetsencryptAgreement = "https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf";
 
 }
 
