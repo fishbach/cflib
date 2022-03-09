d1706fb85117b18c1f467733a0d166aacbcc4594
Christian Fischbach <cfischbach@mac.com>
2022-03-09 14:50:35 +0100

    changed license part 2

--- cflib/net/httprequest.cpp
+++ cflib/net/httprequest.cpp
@@ -3,17 +3,6 @@
  * This file is part of cflib.
  *
  * Licensed under the MIT License.
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * cflib is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with cflib. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "httprequest.h"
