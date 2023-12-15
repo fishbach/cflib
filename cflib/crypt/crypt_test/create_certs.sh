4bbab69622f833aa90600f6af1b783306e511db0
Christian Fischbach <me@cfish.de>
2023-12-15 09:07:26 +0100

    enhanced certificate generation to add SAN

--- cflib/crypt/crypt_test/create_certs.sh
+++ cflib/crypt/crypt_test/create_certs.sh
@@ -14,30 +14,29 @@ echo 01 > crlnumber
 cd ..
 
 # root ca
-openssl genrsa -out rootca_key.pem 2048
-openssl req    -config ../openssl.cnf -new -sha256 -nodes -x509 -key rootca_key.pem -out rootca_crt.pem -days 3650 -subj '/CN=rootca'
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out rootca_key.pem
+openssl req     -config ../openssl.cnf -new -sha256 -x509 -key rootca_key.pem -out rootca_crt.pem -days 3650 -subj '/CN=rootca'
 
 # ca
-openssl genrsa -out ca_key.pem 2048
-openssl req    -config ../openssl.cnf -new -sha256 -key ca_key.pem -out ca_csr.pem -subj '/CN=ca'
-openssl ca     -config ../openssl.cnf -md sha256 -keyfile rootca_key.pem -cert rootca_crt.pem -policy policy_anything -extensions v3_ca -in ca_csr.pem -notext -out ca_crt.pem -days 3650 -batch
-openssl ca     -config ../openssl.cnf -md sha256 -keyfile ca_key.pem -cert ca_crt.pem -gencrl -out ca_crl.pem -crldays 3650
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out ca_key.pem
+openssl req     -config ../openssl.cnf -new -sha256 -key ca_key.pem -out ca_csr.pem -subj '/CN=ca'
+openssl ca      -config ../openssl.cnf -md sha256 -keyfile rootca_key.pem -cert rootca_crt.pem -policy policy_anything -extensions v3_ca -in ca_csr.pem -notext -out ca_crt.pem -days 3650 -batch
+openssl ca      -config ../openssl.cnf -md sha256 -keyfile ca_key.pem -cert ca_crt.pem -gencrl -out ca_crl.pem -crldays 3650
 
 # server
-openssl genrsa -out server_key.pem 2048
-openssl pkcs8  -v2 des3 -topk8 -in server_key.pem -out server_key_pkcs8.pem -passout pass:SuperSecure123
-openssl req    -config ../openssl.cnf -new -sha256 -key server_key.pem -out server_csr.pem -subj '/CN=127.0.0.1'
-openssl ca     -config ../openssl.cnf -md sha256 -keyfile ca_key.pem -cert ca_crt.pem -policy policy_anything -in server_csr.pem -notext -out server_crt.pem -days 3650 -batch
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out server_key.pem
+openssl pkcs8   -v2 des3 -topk8 -in server_key.pem -out server_key_pkcs8.pem -passout pass:SuperSecure123
+openssl req     -config ../openssl.cnf -new -sha256 -key server_key.pem -out server_csr.pem -subj "/CN=127.0.0.1" -addext "subjectAltName = IP:127.0.0.1"
+openssl ca      -config ../openssl.cnf -md sha256 -keyfile ca_key.pem -cert ca_crt.pem -policy policy_anything -in server_csr.pem -notext -out server_crt.pem -days 3650 -batch
 
 # client
-openssl genrsa -out client_key.pem 2048
-openssl pkcs8  -v2 des3 -topk8 -in client_key.pem -out client_key_pkcs8.pem -passout pass:SuperSecure123
-openssl req    -config ../openssl.cnf -new -sha256 -key client_key.pem -out client_csr.pem -subj '/CN=client'
-openssl ca     -config ../openssl.cnf -md sha256 -keyfile ca_key.pem -cert ca_crt.pem -policy policy_anything -in client_csr.pem -notext -out client_crt.pem -days 3650 -batch
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out client_key.pem
+openssl pkcs8   -v2 des3 -topk8 -in client_key.pem -out client_key_pkcs8.pem -passout pass:SuperSecure123
+openssl req     -config ../openssl.cnf -new -sha256 -key client_key.pem -out client_csr.pem -subj '/CN=client'
+openssl ca      -config ../openssl.cnf -md sha256 -keyfile ca_key.pem -cert ca_crt.pem -policy policy_anything -in client_csr.pem -notext -out client_crt.pem -days 3650 -batch
 
 # self signed
-openssl req    -config ../openssl.cnf -x509 -newkey rsa:2048 -passout pass:1234 -sha256 -days 365 -keyout self_key.pem -out self_crt.pem -subj '/CN=localhost'
-openssl pkcs8  -v2 des3 -topk8 -passin pass:1234 -passout pass: -in self_key.pem -out self_key_pkcs8.pem
+openssl req     -config ../openssl.cnf -new -sha256 -x509 -newkey rsa:2048 -nodes -days 365 -keyout self_key.pem -out self_crt.pem -subj '/CN=localhost'
 
 chmod 600 *_key.pem *_key_pkcs8.pem
 
