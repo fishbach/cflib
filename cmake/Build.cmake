c3d27b617dfa6b0f12828b905d0c22621b2a2398
Christian Fischbach <me@cfish.de>
2026-02-02 13:39:12 +0000

    added possibility for own PCH file

--- cmake/Build.cmake
+++ cmake/Build.cmake
@@ -11,22 +11,22 @@ endfunction()
 
 # library
 function(cf_lib lib)
-    cmake_parse_arguments(ARG "ENABLE_MOC;ENABLE_SER;ENABLE_UIC" "" "PUBLIC;PRIVATE;DIRS;OTHER_FILES" ${ARGN})
+    cmake_parse_arguments(ARG "ENABLE_MOC;ENABLE_SER;ENABLE_UIC" "PCH" "PUBLIC;PRIVATE;DIRS;OTHER_FILES" ${ARGN})
 
     cf_find_sources(sources . ${ARG_DIRS} OTHER_FILES ${ARG_OTHER_FILES})
     add_library(${lib} ${sources})
-    cf_configure_target(${lib} ${ARG_ENABLE_MOC} ${ARG_ENABLE_SER} ${ARG_ENABLE_UIC})
+    cf_configure_target(${lib} ${ARG_ENABLE_MOC} ${ARG_ENABLE_SER} ${ARG_ENABLE_UIC} "${ARG_PCH}")
     target_include_directories(${lib} INTERFACE "${PROJECT_SOURCE_DIR}")
     target_link_libraries(${lib} PUBLIC ${ARG_PUBLIC} PRIVATE ${ARG_PRIVATE})
 endfunction()
 
 # application
 function(cf_app app)
-    cmake_parse_arguments(ARG "ENABLE_MOC;ENABLE_SER;ENABLE_UIC;ENABLE_GIT_VERSION;CF_INTERN" "" "DIRS;OTHER_FILES" ${ARGN})
+    cmake_parse_arguments(ARG "ENABLE_MOC;ENABLE_SER;ENABLE_UIC;ENABLE_GIT_VERSION;CF_INTERN" "PCH" "DIRS;OTHER_FILES" ${ARGN})
 
     cf_find_sources(sources . ${ARG_DIRS} OTHER_FILES ${ARG_OTHER_FILES})
     add_executable(${app} ${sources})
-    cf_configure_target(${app} ${ARG_ENABLE_MOC} ${ARG_ENABLE_SER} ${ARG_ENABLE_UIC})
+    cf_configure_target(${app} ${ARG_ENABLE_MOC} ${ARG_ENABLE_SER} ${ARG_ENABLE_UIC} "${ARG_PCH}")
     target_include_directories(${app} PRIVATE .)
     if(NOT ARG_CF_INTERN)
         set_target_properties(${app} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${PROJECT_SOURCE_DIR}/bin")
@@ -61,20 +61,24 @@ endfunction()
 
 # test
 function(cf_test test lib)
-    cmake_parse_arguments(ARG "ENABLE_SER" "" "DIRS" ${ARGN})
+    cmake_parse_arguments(ARG "ENABLE_SER" "PCH" "DIRS" ${ARGN})
 
     cf_find_sources(sources . ${ARG_DIRS})
     add_executable(${test} ${sources})
-    cf_configure_target(${test} TRUE ${ARG_ENABLE_SER} FALSE)
+    cf_configure_target(${test} TRUE ${ARG_ENABLE_SER} FALSE "${ARG_PCH}")
     target_link_libraries(${test} PRIVATE ${lib} Qt5::Test)
     add_test(NAME ${test} COMMAND ${test})
 endfunction()
 
 # configure target
-function(cf_configure_target target enable_moc enable_ser enable_uic)
+function(cf_configure_target target enable_moc enable_ser enable_uic pch)
     # PCH
     if(ENABLE_PCH)
-        target_precompile_headers(${target} REUSE_FROM pch_core)
+        if(pch)
+            target_precompile_headers(${target} PRIVATE ${pch})
+        else()
+            target_precompile_headers(${target} REUSE_FROM pch_core)
+        endif()
     endif()
 
     # Qt generated sources
