5945692a29f75ec35b91a9695679275a39a1accc
Christian Fischbach <me@cfish.de>
2026-04-15 13:08:17 +0200

    no io-ring backend for linux for first (seems buggy)

--- cflib/util/impl/libevimpl.h
+++ cflib/util/impl/libevimpl.h
@@ -14,6 +14,7 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-value"
 
+#define EV_USE_IOURING 0
 #include "../libev/ev.c"
 
 #pragma GCC diagnostic pop
